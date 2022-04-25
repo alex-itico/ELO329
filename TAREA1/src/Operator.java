6053c8dce35db41ab124aafab65fd23a7785d721
Alex <alex.aravenata@usm.cl>
2022-04-25 12:20:26 -0400

    Añade casos de lampara en el operator

--- TAREA1/src/Operator.java
+++ TAREA1/src/Operator.java
@@ -16,19 +16,54 @@ public class Operator {
     public void executeCommands(Scanner in, PrintStream out){
         out.println("Time\t" + cloud.getHeaders());
         while(in.hasNextInt()){
-            int commandTime=in.nextInt();
+            int commandTime=in.nextInt(); //almacena tiempo del segundo
             while (time < commandTime) {
                 out.println(time+"\t"+cloud.getState());
                 cloud.advanceTime(delta);
                 time+=delta;
             }
             String device=in.next(); //Almacena el tipo de dispositivo, si es Lampara o Cortina
-            if (!device.equals("C")) {
+            if (!device.equals("C" /* "L"*/)) {
                 out.println("Unexpected device:" + device);
                 System.exit(-1);
             }
             int channel = in.nextInt();
-            String command=in.next();
+            String command=in.next(); //rescata el tipo de comando que es puede ser UP, DOWN, STOP, ON OFF or RGB
+
+            if(device.equals("C")) { //Si el dispositivo es de tipo Cortina ejecutará sus comandos
+                if (channel == rsControl.getChannel()) {
+                    switch (command.charAt(0)) {
+                        case 'D': //??
+                            // ??
+                        default: out.println("Unexpected command:" + command);
+                            System.exit(-1);
+                    }
+                }
+            }
+            if (device.equals("L")){ //Si el dispositivo es de tipo Lampara ejecturá sus comandos
+                if (channel == lpControl.getChannel()){
+                    switch (command.charAt(0)){
+                        case "N":
+                            //ChangePowerState to ON
+                            break;
+                        case "F":
+                            //ChangePowerState to OFF
+                            break;
+                        case "R":
+                            //
+                            break;
+                        case "G":
+                            //
+                            break;
+                        case "B":
+                            //
+                            break;
+                        default: out.println("Unexpected command:" + command);
+                            System.exit(-1);
+                    }
+                }
+            }
+            /*
             if (channel == rsControl.getChannel()) {
                 switch (command.charAt(0)) {
                     case 'D': //??
@@ -36,7 +71,7 @@ public class Operator {
                     default: out.println("Unexpected command:" + command);
                         System.exit(-1);
                 }
-            }
+            }*/
         }
         out.println(time+"\t"+cloud.getState());
     }
